cc2c1b33f0f3bfabbfe081419badd1b6be1e125a
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-09 18:07:46 -0600

    Move everything from staticComp2 to this repo

---
+++ src/data/apiData.js
@@ -0,0 +1,74 @@
+const userPlaylistData = [ 
+  {
+    userName: 'Sarah Louverts',
+    playlistLink: 'http://music4u.music4ustage.com/p/sara_louverts',
+    playlistName: 'Classic Rock',
+    attachments:  5,
+    views: 2,
+    userImage: 'https://images.pexels.com/photos/706142/pexels-photo-706142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },
+  {
+    userName: 'Ferris Coverns',
+    playlistLink: 'http://music4u.music4ustage.com/p/ferris_coverns',
+    playlistName: 'Techno',
+    attachments:  3,
+    views: 5,
+    userImage: 'https://images.pexels.com/photos/257904/pexels-photo-257904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },
+  {
+    userName: 'Loverly Aims',
+    playlistLink: 'http://music4u.music4ustage.com/p/loverly_aims',
+    playlistName: 'Jazz',
+    attachments:  2,
+    views: 10,
+    userImage: 'https://images.pexels.com/photos/164821/pexels-photo-164821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },
+  {
+    id: 4,
+    userName: 'Evan Gibbons',
+    playlistLink: 'http://music4u.music4ustage.com/p/evan_firlms',
+    playlistName: 'Oldies',
+    attachments:  3,
+    views: 7,
+    userImage: 'https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },
+  {
+    id: 5,
+    userName: 'Patricia Williams',
+    playlistLink: 'http://music4u.music4ustage.com/p/patricia_williams',
+    playlistName: 'Eighties Hits',
+    attachments:  9,
+    views: 4,
+    userImage: 'https://images.pexels.com/photos/3783471/pexels-photo-3783471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },{
+    id: 6,
+    userName: 'Robin Firlth',
+    playlistLink: 'http://music4u.music4ustage.com/p/robin_firlth',
+    playlistName: 'Pop',
+    attachments:  8,
+    views: 5,
+    userImage: 'https://images.pexels.com/photos/144429/pexels-photo-144429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },{
+    id: 7,
+    userName: 'Meryl Jamison',
+    playlistLink: 'http://music4u.music4ustage.com/p/meryl_jamison',
+    playlistName: 'Metal',
+    attachments:  7,
+    views: 10,
+    userImage: 'https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  },
+  {
+    id: 8,
+    userName: 'Pete Evans',
+    playlistLink: 'http://music4u.music4ustage.com/pete_evans',
+    playlistName: 'Alternative',
+    attachments:  4,
+    views: 6,
+    userImage: 'https://images.pexels.com/photos/33597/guitar-classical-guitar-acoustic-guitar-electric-guitar.jpg'
+  },
+]
+
+module.exports = {
+  userPlaylistData
+}
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,31 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+import { userPlaylistData } from './data/apiData'
 import './css/styles.css';
+const cardsContainer = document.getElementById('cards');
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+//Event Listeners==
+window.addEventListener("load", function() {
+  userPlaylistData.forEach((playlist) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <img class="arrow" src="https://cdn-icons-png.flaticon.com/128/122/122646.png">
+      <img src="${playlist.userImage}" alt="User ${playlist.userName} Image">
+      <p class="views">Views</p> 
+      <p class="playlist-views"> ${playlist.views}</p>
+      <h3>${playlist.userName}</h3>
+      <p class="link">${playlist.playlistLink}</p>
+      <p class="date">Thursday 08/07/23 3:44pm</p>
+      <p class="playlist">Playlist: ${playlist.playlistName}</p>
+      <p class="attachments">Attachments</p>
+      <p class="attachment-number">${playlist.attachments}</p>
+    `;
+    cardsContainer.appendChild(card);
+  });
+});
+
+    
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+  
+
+
